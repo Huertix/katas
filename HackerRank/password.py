d3b66784430223c848b9488fa89cf876004fbfb1
David Huerta <d.huerta@carfax.eu>
2017-03-19 09:36:28 +0100

    password submitted

--- HackerRank/password.py
+++ HackerRank/password.py
@@ -1,59 +1,121 @@
 #!/bin/python
 
 import sys
-import itertools
-import string
+# import itertools
+# import string
+import random
 
+
+def generate_password(result, letters):
+    if not result:
+        return list(letters)
+
+    if len(result) <=1:
+        new_result = []
+        for i in letters:
+            new_result.append(result[0]+i)
+        return new_result
+
+    half = len(result)/2
+    left =  generate_password(result[:half], letters)
+    right = generate_password(result[half:], letters)
+    return right + left
+    
 
 
 n = int(raw_input().strip())
 
-# abc = string.lowercase
+if n < 1: n = 1
+if n > 6: n = 6
+
 consonants = 'bcdfghjklmnpqrstvwxz'
 vowels = 'aeiou'
 
-result = itertools.product(vowels, repeat=n)
+letters = [consonants, vowels]
+option = random.randint(0, 1)
+
+result = []
+
+if n <= 1:
+    result = list(consonants) + list(vowels)
+else:
+    for x in range(0, n):
+        result = generate_password(result, letters[option])
+        option = not option
+
+# print len(result)
+for item in result:
+    print item
+
+
+
+
+
+
+
+
+
+
+# # abc = string.lowercase
+# consonants = 'bcdfghjklmnpqrstvwxz'
+# vowels = 'aeiou'
+
+# letters = [consonants, vowels]
+
+# option = random.randint(0, 1)
+
+# result = product(letters[option], letters[not option], repeat=n)
+
+# for item in result:
+#     print item
+
+
+
+
+
+
+
 # result = itertools.izip_longest(consonants, vowels, '-')
-for i in result:
-    word = ''
-    for w in i:
-        word = word + w
+# for i in result:
+#     word = ''
+#     for w in i:
+#         word = word + w
 
-    print word
+#     print word
 
 
-def product(*args, **kwds):
-    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-    pools = map(tuple, args) * kwds.get('repeat', 1)
-    result = [[]]
-    for pool in pools:
-        result = [x+[y] for x in result for y in pool]
-    for prod in result:
-        yield tuple(prod)
+# def product(*args, **kwds):
+#     # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+#     # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+#     pools = map(tuple, args) * kwds.get('repeat', 1)
+#     result = [[]]
+#     for pool in pools:
+#         result = [x+[y] for x in result for y in pool]
+#     for prod in result:
+#         yield tuple(prod)
 
-def izip_longest(*args, **kwds):
-    # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
-    fillvalue = kwds.get('fillvalue')
-    counter = [len(args) - 1]
-    def sentinel():
-        if not counter[0]:
-            raise ZipExhausted
-        counter[0] -= 1
-        yield fillvalue
-    fillers = repeat(fillvalue)
-    iterators = [chain(it, sentinel(), fillers) for it in args]
-    try:
-        while iterators:
-            yield tuple(map(next, iterators))
-    except ZipExhausted:
-        pass
+# def izip_longest(*args, **kwds):
+#     # izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+#     fillvalue = kwds.get('fillvalue')
+#     counter = [len(args) - 1]
+#     def sentinel():
+#         if not counter[0]:
+#             raise ZipExhausted
+#         counter[0] -= 1
+#         yield fillvalue
+#     fillers = repeat(fillvalue)
+#     iterators = [chain(it, sentinel(), fillers) for it in args]
+#     try:
+#         while iterators:
+#             yield tuple(map(next, iterators))
+#     except ZipExhausted:
+#         pass
 
-def izip(*iterables):
-    # izip('ABCD', 'xy') --> Ax By
-    iterators = map(iter, iterables)
-    while iterators:
-        yield tuple(map(next, iterators))
+# def izip(*iterables):
+#     # izip('ABCD', 'xy') --> Ax By
+#     iterators = map(iter, iterables)
+#     while iterators:
+#         yield tuple(map(next, iterators))
 
 
 # result = itertools.product(range(2), repeat=n)
